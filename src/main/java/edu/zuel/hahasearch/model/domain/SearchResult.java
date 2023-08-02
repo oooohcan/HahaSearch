f2cb9b73412c28f0a49207782fbd8f12e06302fa
oooohcan <1489332064@qq.com>
2023-08-02 21:01:33 +0800

    add es搜索结果类

---
+++ src/main/java/edu/zuel/hahasearch/model/domain/SearchResult.java
@@ -0,0 +1,19 @@
+package edu.zuel.hahasearch.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchResult {
+    private Integer id;
+    private String title;
+    private String content;
+    private String website;
+    private String imgUrl;
+    private Date date;
+}
