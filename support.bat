b6cee352776ce9231f25c3f27bdc77ba0ef5edae
josephjostar0 <bohanxiong56@gmail.com>
2023-08-03 15:31:07 +0800

    修改调用方式

--- support.bat
+++ support.bat
@@ -1,4 +1,4 @@
 @echo off
 pip install -r ./spider/requirements.txt || echo "No execution pip install"
 start python ./spider/FlaskApp.py || echo "python spider/FlaskApp.py error"
-start ./elasticsearch/bin/elasticsearch.bat || echo "No execution elasticsearch"
+start call ./elasticsearch/bin/elasticsearch.bat || echo "No execution elasticsearch"
