c4474d3f4e044f184a830d8d3006ed75af982a38
josephjostar0 <bohanxiong56@gmail.com>
2023-08-03 15:26:06 +0800

    修改路径

--- support.bat
+++ support.bat
@@ -1,4 +1,4 @@
 @echo off
 pip install -r ./spider/requirements.txt || echo "No execution pip install"
 start python ./spider/FlaskApp.py || echo "python spider/FlaskApp.py error"
-start ./elastic/bin/elasticsearch.bat || echo "No execution elasticsearch"
+start ./elasticsearch/bin/elasticsearch.bat || echo "No execution elasticsearch"
