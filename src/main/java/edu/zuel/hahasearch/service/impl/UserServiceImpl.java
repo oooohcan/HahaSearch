4bbb2556e8de649c33a5812babb029dce91b7c1c
oooohcan <1489332064@qq.com>
2023-08-05 00:28:49 +0800

    删去无效引用

--- src/main/java/edu/zuel/hahasearch/service/impl/UserServiceImpl.java
+++ src/main/java/edu/zuel/hahasearch/service/impl/UserServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.util.DigestUtils;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
